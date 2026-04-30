169ae8ac35be3cc188c05c0eeea6f770d8cdfd55
Djallexi <63915588+Djallexi@users.noreply.github.com>
2026-04-30 20:43:22 +0200

    Update Aggregate Table.R

--- R/Aggregate Table.R
+++ R/Aggregate Table.R
@@ -1,20 +1,22 @@
-library(data.table)
-
 aggregate_table <- function(dt, 
-                               by_uid    = TRUE, 
-                               by_month  = TRUE,
-                               lat_col   = "lat",
-                               lon_col   = "lon",
-                               uid_col   = "uID",
-                               month_col = "yearmonth",
-                               value_col = "hyads") {
+                                by_uid    = TRUE, 
+                                by_month  = TRUE,
+                                lat_col   = "lat",
+                                lon_col   = "lon",
+                                uid_col   = "uID",
+                                month_col = "yearmonth",
+                                value_col = "hyads") {
   
-  group_keys <- c(
-    lat_col, lon_col,
-    if (by_uid)   uid_col,
-    if (by_month) month_col
-  )
+  # 1. Construction dynamique des colonnes de regroupement
+  group_keys <- c(lat_col, lon_col)
+  if (by_uid)   group_keys <- c(group_keys, uid_col)
+  if (by_month) group_keys <- c(group_keys, month_col)
   
-  dt[, .(value = sum(get(value_col), na.rm = TRUE)), by = group_keys] |>
-    setnames("value", value_col)
+  # 2. Agrégation ultra-rapide avec .SD et .SDcols
+  # Cela dit à data.table : "Applique la fonction sum sur la colonne 'value_col', 
+  # en regroupant par 'group_keys'". 
+  # L'avantage : GForce (l'optimiseur) est activé.
+  res <- dt[, lapply(.SD, sum, na.rm = TRUE), by = group_keys, .SDcols = value_col]
+  
+  return(res)
 }
